390d158f7200337edc5148bfbffcc2d88a1196c7
David R. MacIver <david@drmaciver.com>
2018-08-28 13:50:16 +0100

    I hated literally everything about making this work

--- src/drmnotes/__main__.py
+++ src/drmnotes/__main__.py
@@ -64,6 +64,25 @@ def new_post():
     call([EDITOR, post_file])
 
 
+
+class MathJaxPattern(markdown.inlinepatterns.Pattern):
+
+    def __init__(self, md):
+        markdown.inlinepatterns.Pattern.__init__(self, r'(?<!\\)(\$\$?)(.+?)\2', md)
+
+    def handleMatch(self, m):
+        # Pass the math code through, unmodified except for basic entity substitutions.
+        # Stored in htmlStash so it doesn't get further processed by Markdown.
+        text = cgi.escape(m.group(2) + m.group(3) + m.group(2))
+        return self.markdown.htmlStash.store(text)
+
+
+class MathJaxExtension(markdown.Extension):
+    def extendMarkdown(self, md, md_globals):
+        # Needs to come before escape matching because \ is pretty important in LaTeX
+        md.inlinePatterns.add('mathjax', MathJaxPattern(md), '<escape')
+
+
 class MathJaxPattern(markdown.inlinepatterns.Pattern):
 
     def __init__(self, md):
@@ -112,6 +131,8 @@ def build(rebuild):
 
         with open(source) as i:
             source_text = i.read()
+
+        source_text = source_text.replace(r"\(", "$").replace(r"\)", "$")
 
         source_html = md(source_text)
         soup = BeautifulSoup(source_html, 'html.parser')
