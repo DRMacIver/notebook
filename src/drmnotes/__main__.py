6f89e75e2869e29d5b3c7587589dfd37a0472294
David R. MacIver <david@drmaciver.com>
2020-03-06 07:44:03 +0000

    Fix type indicator in feed

--- src/drmnotes/__main__.py
+++ src/drmnotes/__main__.py
@@ -366,7 +366,7 @@ def do_build(rebuild=False, full=True, name=''):
         fe.id('https://notebook.drmaciver.com' + post.url)
         fe.link(href='https://notebook.drmaciver.com' + post.url)
         fe.title(post.title or post.name)
-        fe.content(post.body)
+        fe.content(post.body, type="html")
         updated = subprocess.check_output([
         "git", "log", "-1", '--date=iso8601', '--format="%ad"', "--", post.original_file,
         ]).decode('ascii').strip().strip('"')
